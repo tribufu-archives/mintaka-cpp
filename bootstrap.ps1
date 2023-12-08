64fdd1c345a64ac1f4b65b970f0be787611fbc7e
Guilherme Werner <guilherme.werner@tribufu.com>
2023-12-08 13:07:24 -0300

    Update premake scripts

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,13 +2,10 @@
 
 if ($IsWindows)
 {
-    & "./vendor/premake/windows/premake5.exe" "vs2022"
 }
 elseif ($IsMacOS)
 {
-    & "./vendor/premake/mac/premake5" "xcode4"
 }
 elseif ($IsLinux)
 {
-    & "./vendor/premake/linux/premake5" "gmake2"
 }
