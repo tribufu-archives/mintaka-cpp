743a3a1583aefa026f5c6da5f7fe1fa6236215d9
Guilherme Werner <guilherme.werner@tribufu.com>
2023-12-08 12:10:00 -0300

    Update msbuild.ps1

--- scripts/msbuild.ps1
+++ scripts/msbuild.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 
-echo "Building for x86_64-pc-windows-msvc"
+echo "Building for windows-x86_64"
 cargo build -p mintaka-native
 
 msbuild /p:Configuration="debug" /p:Platform="x86_64-pc-windows-msvc"
