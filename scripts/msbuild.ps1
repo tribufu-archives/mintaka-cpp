dd7b0bacd9f090cbaaace8846b04d5e6f2295f02
Guilherme Werner <guilherme.werner@tribufu.com>
2023-12-08 12:10:05 -0300

    Update msbuild.ps1

--- scripts/msbuild.ps1
+++ scripts/msbuild.ps1
@@ -3,4 +3,13 @@
 echo "Building for windows-x86_64"
 cargo build -p mintaka-native
 
+New-Item "bin/windows/x86_64" -ItemType Directory -Force
+Remove-Item -Path "bin/windows/x86_64/*" -Force
+
+Copy-Item -Path "target/debug/mintaka_native.dll" -Destination "bin/windows/x86_64" -Force
+Copy-Item -Path "target/debug/mintaka_native.dll.exp" -Destination "bin/windows/x86_64" -Force
+Copy-Item -Path "target/debug/mintaka_native.dll.lib" -Destination "bin/windows/x86_64" -Force
+Copy-Item -Path "target/debug/mintaka_native.lib" -Destination "bin/windows/x86_64" -Force
+Copy-Item -Path "target/debug/mintaka_native.pdb" -Destination "bin/windows/x86_64" -Force
+
 msbuild /p:Configuration="debug" /p:Platform="x86_64-pc-windows-msvc"
