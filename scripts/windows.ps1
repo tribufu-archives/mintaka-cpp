8a2b8f41ff5ab9283e8031a9a18e110a5d0c44a9
Guilherme Werner <guilherme.werner@tribufu.com>
2023-12-08 17:01:42 -0300

    Update windows.ps1

--- scripts/windows.ps1
+++ scripts/windows.ps1
@@ -5,19 +5,19 @@ function Build-For-Architecture {
         [string]$Arch
     )
 
-    $OutputDir = "lib/windows/$Arch"
+    $OutputDir = "bin/windows/$Arch"
     Write-Output "Building for $Arch"
 
-    cargo build -p mintaka-native --target $Arch-pc-windows-msvc --release
+    cargo build -p mintaka-native --target $Arch-pc-windows-msvc
 
     if (-not (Test-Path $OutputDir)) {
         New-Item -ItemType Directory -Force -Path $OutputDir
     }
 
     Remove-Item -Path "$OutputDir/*" -Force
-    Copy-Item -Path "target/$Arch-pc-windows-msvc/release/mintaka_native.dll" -Destination $OutputDir -Force
-    Copy-Item -Path "target/$Arch-pc-windows-msvc/release/mintaka_native.dll.lib" -Destination $OutputDir -Force
-    Copy-Item -Path "target/$Arch-pc-windows-msvc/release/mintaka_native.lib" -Destination $OutputDir -Force
+    Copy-Item -Path "target/$Arch-pc-windows-msvc/debug/mintaka_native.dll" -Destination $OutputDir -Force
+    Copy-Item -Path "target/$Arch-pc-windows-msvc/debug/mintaka_native.dll.lib" -Destination $OutputDir -Force
+    Copy-Item -Path "target/$Arch-pc-windows-msvc/debug/mintaka_native.lib" -Destination $OutputDir -Force
 }
 
 $architectures = @("aarch64", "i686", "x86_64")
