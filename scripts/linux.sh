170a4e8264cae3c440f13624ad356fd33676ec21
Guilherme Werner <guilherme.werner@tribufu.com>
2023-12-08 17:01:41 -0300

    Update linux.sh

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -2,7 +2,7 @@
 
 build_for_architecture() {
     ARCH=$1
-    OUTPUT_DIR="lib/linux/$ARCH"
+    OUTPUT_DIR="bin/linux/$ARCH"
 
     echo "Building for linux-$ARCH"
     mkdir -p $OUTPUT_DIR
@@ -13,10 +13,10 @@ build_for_architecture() {
         -v ~/.cargo/registry:/usr/local/cargo/registry \
         -w /src/mintaka \
         reg.tribufu.com/cross:linux-$ARCH \
-        cargo build --target $ARCH-unknown-linux-gnu --package mintaka-native --release
+        cargo build --target $ARCH-unknown-linux-gnu --package mintaka-native
 
-    cp target/$ARCH-unknown-linux-gnu/release/libmintaka_native.so $OUTPUT_DIR
-    cp target/$ARCH-unknown-linux-gnu/release/libmintaka_native.a $OUTPUT_DIR
+    cp target/$ARCH-unknown-linux-gnu/debug/libmintaka_native.so $OUTPUT_DIR
+    cp target/$ARCH-unknown-linux-gnu/debug/libmintaka_native.a $OUTPUT_DIR
 }
 
 architectures=("aarch64" "i686" "x86_64")
